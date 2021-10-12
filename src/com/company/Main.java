c852d7fe58d7796f2c42bcc1473054e7942a2858
Louis Clark <lclark@softwareinstitute>
2021-10-12 17:14:45 +0100

    This one icludes an enhanced for loop in the main file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,14 +16,20 @@ public class Main {
         byte test_float2 = (byte)test_float;
         System.out.println(test_float2);*/
 
-        int[] myNum = {10, 20, 30, 40,2,3,4,5,6,7,8,6,5,4,3,2,1,5};
-
-
+        int[] myNum = {10,20,30,40,2,3,4,5,6,7,8,6,5,4,3,2,1,5};
         System.out.println(Arrays.toString(myNum));
 
 
-        for (int i = 0; i < 10; i++ ){
+        for (int i = 0; i < 10; i++ ){  //this replaces the numbers with myNum with their indices
             myNum[i] = i;
+        }
+        System.out.println(Arrays.toString(myNum));
+
+        int i = 0;
+        for (int k: myNum){             // this loop adds 1 to each element. WOw!
+            myNum[i] = k + 1;
+            i++;
+
         }
         System.out.print(Arrays.toString(myNum));
     }
